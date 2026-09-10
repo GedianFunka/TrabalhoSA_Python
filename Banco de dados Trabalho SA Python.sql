3d590904d37128bf1aa73e13febe06a30d19dd85
gedianfunka <gediangabrielf@gmail.com>
2026-09-10 09:02:54 -0300

    Banco de dados corrijido

--- Banco de dados Trabalho SA Python.sql
+++ Banco de dados Trabalho SA Python.sql
@@ -14,14 +14,14 @@ horario varchar(5),
 status enum("Agendado", "Concluido", "Cancelado")
 );
 
-insert into agendamentos(cliente, telefone, servico, preco, barbeiro, data, horario, status)
-values("Gedian", "(47)992165428", "Corte de cabelo", 55.00, "Lucas", "2026-09-20", "10:50", "Agendado"),
-("Kauã", "(47)991544034", "Corte de cabelo e barba", 75.00, "Otavio", "2026-09-25", "15:30", "Agendado"),
-("Weslley", "(47)33752968", "Corte de cabelo", 55.00, "André", "2026-09-08", "9:00", "Concluido"),
-("Willian", "(47)998476952", "Corte de cabelo", 55.00, "Lucas", "2026-09-01", "10:00", "Cancelado"),
-("Guilherme", "(47)996213498", "Barba", 30.00, "Enzo", "2026-09-05", "Concluido"),
-("Nicolas", "(47)997456213", "Corte de cabelo e barba", 75.00, "André", "2026-10-10", "Agendado"),
-("Kelvin", "(47)996523149", "Barba", 30.00, "Lucas", "2026-09-29", "Agendado"),
-("Henrique", "(47)995642879", "Corte de cabelo", 55.00, "Otavio", "2026-10-01", "Cancelado")
+iINSERT INTO agendamentos (cliente, telefone, servico, preco, barbeiro, data, horario, status)
+Values('Gedian', '(47)992165428', 'Corte de cabelo', 55.00, 'Lucas', '2026-09-20', '10:50', 'Agendado'),
+('Kauã', '(47)991544034', 'Corte de cabelo e barba', 75.00, 'Otavio', '2026-09-25', '15:30', 'Agendado'),
+('Weslley', '(47)33752968', 'Corte de cabelo', 55.00, 'André', '2026-09-08', '09:00', 'Concluido'),
+('Willian', '(47)998476952', 'Corte de cabelo', 55.00, 'Lucas', '2026-09-01', '10:00', 'Cancelado'),
+('Guilherme', '(47)996213498', 'Barba', 30.00, 'Enzo', '2026-09-05', '14:00', 'Concluido'),
+('Nicolas', '(47)997456213', 'Corte de cabelo e barba', 75.00, 'André', '2026-10-10', '11:00', 'Agendado'),
+('Kelvin', '(47)996523149', 'Barba', 30.00, 'Lucas', '2026-09-29', '16:00', 'Agendado'),
+('Henrique', '(47)995642879', 'Corte de cabelo', 55.00, 'Otavio', '2026-10-01', '17:30', 'Cancelado');
 
-
+select * from agendamentos;
